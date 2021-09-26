1503b11fe2812502707786792355a68ce52e949d
Brandon George <geobrandon96@gmail.com>
2021-09-26 18:20:39 -0400

    added a way to reset the state without refreshing the screen

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -1,15 +1,18 @@
 import './TicTacToe.css';
 import {Board} from './Board/Board';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {setNewGame} from "./playerSlice";
 
 function TicTacToe() {
+    const dispatch = useDispatch();
     const {player, currentPlayerPiece, isWinner} = useSelector(state => state.player);
-    const continueGame = <h3>It is player {player}'s turn, using character: {currentPlayerPiece}</h3>;
+    const continueGame = <h3>It is player {player}'s turn, using character: {currentPlayerPiece}.</h3>;
     const declareWinner = <h3>Congratulations to player {player} for winning the game!</h3>;
 
     return (
         <div className="TicTacToe">
             <h2>TicTacToe</h2>
+            <button onClick={() => dispatch(setNewGame())}>Start New Game</button>
             {isWinner ? declareWinner : continueGame}
             <Board/>
         </div>
--- src/components/TicTacToe/playerSlice.js
+++ src/components/TicTacToe/playerSlice.js
@@ -1,28 +1,38 @@
 import {createSlice} from '@reduxjs/toolkit'
 import {checkWin, newBoard} from "./Game/GameLogic";
 
+const newGame = {
+    player: 1,
+    currentPlayerPiece: 'X',
+    playerPieces: ['X', 'O'],
+    isWinner: false,
+    gameBoard: newBoard()
+}
+
 export const playerSlice = createSlice({
     name: 'player',
-    initialState: {
-        player: 1,
-        currentPlayerPiece: 'X',
-        playerPieces: ['X', 'O'],
-        isWinner: false,
-        gameBoard: newBoard()
-    },
+    initialState: newGame,
     reducers: {
         nextPlayer: (state, action) => {
             const currentPlayer = state.player;
             const nextToPlay = (currentPlayer % 2) + 1;
-            state.player = nextToPlay;
-            state.currentPlayerPiece = state.playerPieces[nextToPlay - 1];
             state.gameBoard[action.payload.place] = currentPlayer - 1;
             state.isWinner = checkWin([...state.gameBoard]);
+            if (!state.isWinner) {
+                state.player = nextToPlay;
+                state.currentPlayerPiece = state.playerPieces[nextToPlay - 1];
+            }
+        },
+        setNewGame: state => {
+            state.player = newGame.player;
+            state.currentPlayerPiece = newGame.currentPlayerPiece;
+            state.playerPieces = newGame.playerPieces;
+            state.isWinner = newGame.isWinner;
+            state.gameBoard = newGame.gameBoard;
         }
     },
 })
 
-// Action creators are generated for each case reducer function
-export const {nextPlayer} = playerSlice.actions
+export const {nextPlayer, setNewGame} = playerSlice.actions
 
 export default playerSlice.reducer
